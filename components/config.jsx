bf525ad871cf3f2784b0497774a916d38ea9e980
christopher-2000 <christopher.geever@gmail.com>
2022-05-05 01:22:38 +0530

    config.jsx file added

---
+++ components/config.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAz7NAs51iyKn7fHItpzFSxyEI6NKakPwE",
+  authDomain: "accident-reporting-app.firebaseapp.com",
+  projectId: "accident-reporting-app",
+  storageBucket: "accident-reporting-app.appspot.com",
+  messagingSenderId: "631328755010",
+  appId: "1:631328755010:web:c3177df3f7380afb298cdb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
