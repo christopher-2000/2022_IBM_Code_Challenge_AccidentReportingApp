35de9dfc1e8207b04f4d6365806b206096b5f8ee
christopher-2000 <christopher.geever@gmail.com>
2022-05-05 22:21:07 +0530

    Testing Connectivity with Firebase - Success

--- src/screens/BottomTabScreens/Cases/index.js
+++ src/screens/BottomTabScreens/Cases/index.js
@@ -1,11 +1,29 @@
 import { View, Text ,Image,FlatList} from 'react-native'
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
+import { db } from '../../../../components/config';
+import { ref, onValue } from 'firebase/database';
+import { useState } from 'react/cjs/react.production.min';
+import { Button } from 'react-native-web';
+
 
 export default function Cases(){
+  //const [test_case, setCase] = useState({})
+
+  function readData(){
+    const starCountRef = ref(db, 'Accidents/CaseID');
+    onValue(starCountRef, (snapshot) => {
+      const data = snapshot.val();
+      console.log(data);
+      //setCase(data,test_case);
+});
+
+  }
+
   return (
     <View >
-        <StatusBar style="light" backgroundColor="black"/>
+      <Text onPress={readData}> hello </Text>
+      <StatusBar style="light" backgroundColor="black"/>
     </View>
   )
 }
